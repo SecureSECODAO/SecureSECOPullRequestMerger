d7c60d5c7a9e64f33456a180c776c68fe1b5f8fc
Mike <23509561+mik-0@users.noreply.github.com>
2023-04-21 15:42:03 +0200

    feat:

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import { createPublicClient, http } from "viem";
 import { mainnet, polygonMumbai } from "viem/chains";
+import { Octokit } from "@octokit/rest";
 import { abi } from "./abi.js";
 import * as dotenv from "dotenv";
 dotenv.config();
+
+// Create a GitHub client so we can merge the pull request
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+  userAgent: "SecureSECOPullRequestMerger",
+});
 
 // Create a client so we can read from the blockchain
 const client = createPublicClient({
@@ -15,15 +22,34 @@ console.log(`Contract address: ${process.env.CONTRACT_ADDRESS}`);
 // Detect the MergePullRequest event. This is emitted when a pull request needs to be merged,
 //  as a result of the execution of the mergePullRequest function. That function is called
 //  when a proposal from the DAO is accepted and executed with a specific action.
-const unwatch = client.watchContractEvent({
+client.watchContractEvent({
   address: process.env.CONTRACT_ADDRESS as any,
   abi,
   eventName: "MergePullRequest",
-  onLogs: (logs) => {
+  onLogs: async (logs) => {
     console.log(logs);
     for (let log of logs) {
-      // Extract the owner, repo and pull_number from the event
-      const { owner, repo, pull_number } = (log as any).args;
+      try {
+        // Extract the owner, repo and pull_number from the event
+        const { owner, repo, pull_number } = (log as any).args;
+
+        // Merge the pull request
+        const res = await octokit.pulls.merge({
+          owner,
+          repo,
+          pull_number,
+        });
+
+        if (res.data && res.data.merged === true) {
+          console.log(
+            `Pull request merged successfully: (${owner}/${repo}#${pull_number})`
+          );
+        } else {
+          console.log("Pull request not merged: \n", res);
+        }
+      } catch (error) {
+        console.log("Could not merge pull request: \n", error);
+      }
     }
   },
 });
